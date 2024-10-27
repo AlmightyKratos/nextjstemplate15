a249ace58644ef087d020cb8177e0cad5f3ec75f
Ethan McPherson <emcpherson121@gmail.com>
2024-10-28 09:58:59 +1100

    layout changes

--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -12,7 +12,7 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { Button } from "@/components/ui/button"
 
-export function ModeToggle() {
+export function ThemeToggle() {
   const { setTheme } = useTheme()
 
   return (
